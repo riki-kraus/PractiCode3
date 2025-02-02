2f569e437a4272daa498bfd5656125cce846f24c
riki-kraus <maof5728@gmail.com>
2025-02-02 02:39:30 +0200

    comm7

--- my-express-app/server.js
+++ my-express-app/server.js
@@ -1,18 +1,6 @@
-import express from 'express';
-import api from 'api';
+import renderApi from '@api/render-api';
 
-const app = express();
-
-const apiKey = 'rnd_dfz0iQh1VXR4EhUL5geo7OImmCcB';
-
-app.get('/apps', (req, res) => {
-  api.auth(apiKey);
-  api.listServices({ includePreviews: 'true', limit: '20' })
-    .then(({ data }) => {
-      res.json(data); 
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err.message });
-    });
-});
-
+renderApi.auth('rnd_dfz0iQh1VXR4EhUL5geo7OImmCcB');
+renderApi.listServices({includePreviews: 'true', limit: '20'})
+  .then(({ data }) => console.log(data))
+  .catch(err => console.error(err));
